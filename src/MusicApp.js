b366c52853edb044704f5f44d7ee1b248b0c61c3
michellesegv <michelle@laboratoria.la>
2018-03-06 17:07:25 -0500

    intento 1

--- src/MusicApp.js
+++ src/MusicApp.js
@@ -3,24 +3,14 @@ import './MusicApp.css';
 import Music from './Music';
 
 
-class MusicApp extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Music 
-          artista = 'Shakira'
-          canciones = {['ojos Asi','Inevitable','tu']}
-          img = 'shakira.jpg'
-          bio = 'Shakira Isabel Mebarak Ripoll​​ es una cantautora, productora discográfica, bailarina, modelo, empresaria, actriz, embajadora de buena voluntad de la UNICEF y filántropa colombiana'
-          /> 
-      </div>
-    );
-  }
-}
+// class MusicApp extends Component {
 
-  
-   
-  
-
-
-export default MusicApp;
+//   render() {
+//     return (
+//       <Music>
+//         {artista}
+//       </Music>
+//     );
+//   }
+// }
+// export default MusicApp;
